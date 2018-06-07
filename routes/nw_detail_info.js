3a82e85aeed931bd2f1e25d8ad584bd9dbafdc5a
Anthrino <jerinjohn101@gmail.com>
2018-06-07 18:06:03 +0530

    Add subnet info route and template

---
+++ routes/nw_detail_info.js
@@ -0,0 +1,52 @@
+/*
+© Anthrino > Network detailed info route
+*/
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var template_render = require('../lib/render_template.js');
+var authorize = require('../lib/authorize.js');
+
+router.get('/', function (req, res, next) {
+
+    var pools, subnet;
+    let id = req.query.id.replace('?v_ajax', '');
+
+    utilization = parseFloat(kea_stats['subnet[' + id + '].assigned-addresses'][0][0]
+        / kea_stats['subnet[' + id + '].total-addresses'][0][0]) * 100;
+    if (isNaN(utilization))
+        utilization = 0;
+        
+    kea_config['Dhcp4']['subnet4'].forEach(s => {
+        if (s.id == id) {
+            pools = s.pools;
+            subnet = s.subnet;
+        }
+    });
+
+    
+    pool_range = '';
+    if (pools) {
+        pools.forEach(p => {
+            pool_range += p.pool + '<br>';
+        });
+        if (pool_range == '')
+            pool_range = ' - undefined - ';
+    }
+    console.log(pool_range, subnet);
+
+    var content = "";
+    content = template_render.get_template("nw_detail_info");
+
+    content = template_render.set_template_variable(content, "title", "Subnet [" + subnet + "] Information");
+    content = template_render.set_template_variable(content, "subnet", subnet);
+    content = template_render.set_template_variable(content, "utilzn", utilization);
+    content = template_render.set_template_variable(content, "pools", pool_range)
+    content = template_render.set_template_variable(content, "assgn_addr", kea_stats['subnet[' + id + '].assigned-addresses'][0][0]);
+    content = template_render.set_template_variable(content, "total_addr", kea_stats['subnet[' + id + '].total-addresses'][0][0]);
+    content = template_render.set_template_variable(content, "free_addr", kea_stats['subnet[' + id + '].total-addresses'][0][0] - kea_stats['subnet[' + id + '].assigned-addresses'][0][0]);
+
+    res.send(template_render.get_index_template(content, req.url));
+});
+
+module.exports = router;
