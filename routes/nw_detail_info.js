628f8c4c59e443ae49f4b2f6be2f3e8375661bb7
Anthrino <jerinjohn101@gmail.com>
2018-06-11 11:39:27 +0530

    Correct details page utilizn bar

--- routes/nw_detail_info.js
+++ routes/nw_detail_info.js
@@ -16,7 +16,17 @@ router.get('/', function (req, res, next) {
         / kea_stats['subnet[' + id + '].total-addresses'][0][0]) * 100;
     if (isNaN(utilization))
         utilization = 0;
-        
+
+    utilization_color = 'green';
+
+    if (utilization >= 80)
+        utilization_color = 'orange';
+    if (utilization >= 90)
+        utilization_color = 'red';
+
+    utilization_html = '<div class="progress-bar bg-' + utilization_color +
+        '" role="progressbar" aria-valuenow="62" aria-valuemin="0" aria-valuemax="100" style="width: ' + utilization + '%"></div>';
+
     kea_config['Dhcp4']['subnet4'].forEach(s => {
         if (s.id == id) {
             pools = s.pools;
@@ -24,7 +34,7 @@ router.get('/', function (req, res, next) {
         }
     });
 
-    
+
     pool_range = '';
     if (pools) {
         pools.forEach(p => {
@@ -40,7 +50,7 @@ router.get('/', function (req, res, next) {
 
     content = template_render.set_template_variable(content, "title", "Subnet [" + subnet + "] Information");
     content = template_render.set_template_variable(content, "subnet", subnet);
-    content = template_render.set_template_variable(content, "utilzn", utilization);
+    content = template_render.set_template_variable(content, "utilzn_bar", utilization_html);
     content = template_render.set_template_variable(content, "pools", pool_range)
     content = template_render.set_template_variable(content, "assgn_addr", kea_stats['subnet[' + id + '].assigned-addresses'][0][0]);
     content = template_render.set_template_variable(content, "total_addr", kea_stats['subnet[' + id + '].total-addresses'][0][0]);
