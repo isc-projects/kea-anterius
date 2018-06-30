c2bc1704c6eb92c1cb6ce1dbde4a2d34cfe857e3
Anthrino <jerinjohn101@gmail.com>
2018-06-30 22:44:34 +0530

    Update config snapshot listing and view fn

--- public/assets/js/anterius-pages/config-snapshots.js
+++ public/assets/js/anterius-pages/config-snapshots.js
@@ -1,13 +1,13 @@
 function view_snapshot(snapshot) {
-	$.post( "/dhcp_config_snapshot_view", "snapshot=" + encodeURIComponent(snapshot), function( data ) {
+	$.post("/dhcp_config_snapshot_view", "snapshot=" + encodeURIComponent(snapshot), function (data) {
 
 		$('#snapshot_grid').fadeOut(100).fadeIn(100);
 		$('#snapshot_name').html("Snapshot '" + snapshot + "'");
 		$('#snapshot_body').html('<div id="snapshot" style="width:100%; height:800px; color: #95cd24">' + data + '</div>');
 
-        $('html, body').animate({
-            scrollTop: $("#snapshot_grid").offset().top
-        }, 500);
+		$('html, body').animate({
+			scrollTop: $("#snapshot_grid").offset().top
+		}, 500);
 
 		config_snapshot = ace.edit("snapshot");
 		config_snapshot.setTheme("ace/theme/terminal");
--- routes/dhcp_config_snapshots.js
+++ routes/dhcp_config_snapshots.js
@@ -3,11 +3,14 @@ var router = express.Router();
 var fs = require('fs');
 var template_render = require('../lib/render_template.js');
 var authorize = require('../lib/authorize.js');
+var mkdirp = require('mkdirp');
 
-function human_time (time){
+var bkp_dir = "config_backups";
+
+function human_time(time) {
     var time = new Date(time);
     var year = time.getFullYear();
-    var month = time.getMonth()+1;
+    var month = time.getMonth() + 1;
     var date1 = time.getDate();
     var hour = time.getHours();
     var minutes = time.getMinutes();
@@ -16,44 +19,42 @@ function human_time (time){
     var hour = time.getHours();
     var minute = time.getMinutes();
     var amPM = (hour > 11) ? "PM" : "AM";
-    if(hour > 12) {
+    if (hour > 12) {
         hour -= 12;
-    } else if(hour == 0) {
+    } else if (hour == 0) {
         hour = "12";
     }
-    if(minute < 10) {
+    if (minute < 10) {
         minute = "0" + minute;
     }
 
-    return year + "-" + month+"-"+date1+" "+hour + ":" + minute + ' ' + amPM;
+    return year + "-" + month + "-" + date1 + " " + hour + ":" + minute + ' ' + amPM;
 }
 
-router.get('/', authorize.auth, function(req, res, next) {
+router.post('/', authorize.auth, function (req, res, next) {
 
-	var content = "";
+    /* Create snapshot bckp folder if missing */
+    try {
 
-	content = template_render.get_template("dhcp_config_snapshots");
+        if (!fs.existsSync(bkp_dir)) {
+            fs.mkdirSync(bkp_dir);
+        }
+        var request = req.body;
+        var json_file = require('jsonfile');
+        var timestamp = new Date().getTime();
+        var ss_filename = anterius_config.current_server + "_conf_snap_" + timestamp;
 
-	/* Read Config */
-	var json_file = require('jsonfile');
+        dhcp_config_file = JSON.parse(request.dhcp_config_file);
+        // console.log(ss_filename, human_time(timestamp), dhcp_config_file);
 
-	content = template_render.set_template_variable(content, "title", "DHCP Config Snaphots");
+        json_file.writeFile(bkp_dir + '/' + ss_filename, dhcp_config_file, { spaces: 2 });
 
-	var backups = '';
-	fs.readdirSync("./config_backups").forEach(function(file) {
-        var stats = fs.statSync("./config_backups/" + file);
-        var mtime = human_time(stats.mtime);
+    } catch (err) {
+        console.error(err);
+    }
 
-		backups = backups + "<tr><td><a style='cursor:pointer;' onclick='view_snapshot(" + JSON.stringify(file) + ")'>" + file + '</a></td><td>' + mtime + '</td></tr>';
-	});
+    res.send({ "message": 'Config snapshot created @ <br>< ' + human_time(timestamp) + ' >' });
 
-	if(backups == ''){
-		backups = 'There are no snapshots present at this time...';
-	}
-
-	content = template_render.set_template_variable(content, "c_content", backups);
-
-	res.send(template_render.get_index_template(content, req.url));
 });
 
 module.exports = router;
