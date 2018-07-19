53336b525d8242a12e8aa0e2a680647687725514
Anthrino <jerinjohn101@gmail.com>
2018-07-20 01:26:59 +0530

    Add network addn request processor

--- public/assets/js/anterius-pages/config-update.js
+++ public/assets/js/anterius-pages/config-update.js
@@ -12,7 +12,7 @@ var Range = ace.require('ace/range').Range;
 var config_copy = dhcp_config.session.doc.getAllLines();
 var markedList = [];
 
-function gen_dhcp_config(server, nw_id, nw_type, subnet_list) {
+function gen_dhcp_config(mode, svr_tag, sn_tag, nw_id, nw_type, subnet_list) {
 
 	/* Fetch config form options as query string */
 	// dhcp_config_form_data = get_form_query_string("dhcp_config_form");
@@ -23,95 +23,101 @@ function gen_dhcp_config(server, nw_id, nw_type, subnet_list) {
 
 	/* Instantiate a temp test file and Copy to identify file changes*/
 	config_copy = dhcp_config.session.doc.getAllLines();
-	test_config_file = server.server_config;
+	// console.log(dhcp_config.getValue())
+	test_config_file = JSON.parse(dhcp_config.getValue());
 
-	// console.log(test_config_file[server.svr_tag][server.sn_tag][0]);
+	// console.log(test_config_file[svr_tag][sn_tag][0]);
 	// console.log(subnet_list);
 
-	if (nw_type == 'reservations') {
+	if (req.query.mode == 'add' && nw_id == -1) {
 
-		hr_addr = nw_id.split(':')[0];
-		subnet_id = nw_id.split(':')[1];
-
-		target_sn_list = [];
-
-		/* Check if subnet lies under shared nw */
-		if (subnet_list[subnet_id - 1].shared_nw_name)
-			test_config_file[server.svr_tag]['shared-networks'].forEach(shnw => {
-				if (shnw.name == subnet_list[subnet_id - 1].shared_nw_name)
-					target_sn_list = shnw[server.sn_tag];
-			});
-		else
-			target_sn_list = test_config_file[server.svr_tag][server.sn_tag];
-
-		target_sn_list.forEach(s => {
-			if (s.id == subnet_id) {
-				s['reservations'].forEach(h => {
-					if (h['ip-address'] == hr_addr)
-						configformData.forEach(fd => {
-							if (fd.value != null && fd.value != 'undefined') {
-								if (!isNaN(fd.value))
-									fd.value = parseInt(fd.value);
-								if (fd.name.includes('_')) {
-									tags = fd.name.split('_');
-									if (h[tags[0]][tags[1]] != fd.value) {
-										h[tags[0]][tags[1]] = fd.value;
-									}
-								}
-								else
-									if (h[fd.name] != fd.value) {
-										h[fd.name] = fd.value;
-									}
-							}
-						});
-				});
-				// TODO: make effecient
-				// break;
-				console.log(s);
-			}
-		});
-	} else {
-
-		target_sn_list = test_config_file[server.svr_tag][nw_type];
-
-		if (nw_type == 'shared-networks')
-			x = 'name';
-		else {
-			if (subnet_list[nw_id - 1].shared_nw_name)
-				test_config_file[server.svr_tag]['shared-networks'].forEach(shnw => {
-					if (shnw.name == subnet_list[nw_id - 1].shared_nw_name)
-						target_sn_list = shnw[server.sn_tag];
-				});
-			x = 'id';
-		}
-		// console.log(target_sn_list);
-
-		target_sn_list.forEach(s => {
-			if (s[x] == nw_id) {
-				configformData.forEach(fd => {
-					if (fd.value != null && fd.value != 'undefined') {
-						if (!isNaN(fd.value))
-							fd.value = parseInt(fd.value);
-						if (fd.name.includes('_')) {
-							tags = fd.name.split('_');
-							if (s[tags[0]][tags[1]] != fd.value) {
-								s[tags[0]][tags[1]] = fd.value;
-							}
-						}
-						else
-							if (s[fd.name] != fd.value) {
-								s[fd.name] = fd.value;
-							}
-					}
-				});
-				// TODO: make effecient
-				// break;
-				// console.log(s)
-				target_sn_list.push();
-			}
-		});
 	}
 
+	else {
+		if (nw_type == 'reservations') {
+
+			hr_addr = nw_id.split(':')[0];
+			subnet_id = nw_id.split(':')[1];
+
+			target_sn_list = [];
+
+			/* Check if subnet lies under shared nw */
+			if (subnet_list[subnet_id - 1].shared_nw_name)
+				test_config_file[svr_tag]['shared-networks'].forEach(shnw => {
+					if (shnw.name == subnet_list[subnet_id - 1].shared_nw_name)
+						target_sn_list = shnw[sn_tag];
+				});
+			else
+				target_sn_list = test_config_file[svr_tag][sn_tag];
+
+			target_sn_list.forEach(s => {
+				if (s.id == subnet_id) {
+					s['reservations'].forEach(h => {
+						if (h['ip-address'] == hr_addr)
+							configformData.forEach(fd => {
+								if (fd.value != null && fd.value != 'undefined') {
+									if (!isNaN(fd.value))
+										fd.value = parseInt(fd.value);
+									if (fd.name.includes('_')) {
+										tags = fd.name.split('_');
+										if (h[tags[0]][tags[1]] != fd.value) {
+											h[tags[0]][tags[1]] = fd.value;
+										}
+									}
+									else
+										if (h[fd.name] != fd.value) {
+											h[fd.name] = fd.value;
+										}
+								}
+							});
+					});
+					// TODO: make effecient
+					// break;
+					// console.log(s);
+				}
+			});
+		} else {
+
+			target_sn_list = test_config_file[svr_tag][nw_type];
+
+			if (nw_type == 'shared-networks')
+				x = 'name';
+			else {
+				if (subnet_list[nw_id - 1].shared_nw_name)
+					test_config_file[svr_tag]['shared-networks'].forEach(shnw => {
+						if (shnw.name == subnet_list[nw_id - 1].shared_nw_name)
+							target_sn_list = shnw[sn_tag];
+					});
+				x = 'id';
+			}
+			// console.log(target_sn_list);
+
+			target_sn_list.forEach(s => {
+				if (s[x] == nw_id) {
+					configformData.forEach(fd => {
+						if (fd.value != null && fd.value != 'undefined') {
+							if (!isNaN(fd.value))
+								fd.value = parseInt(fd.value);
+							if (fd.name.includes('_')) {
+								tags = fd.name.split('_');
+								if (s[tags[0]][tags[1]] != fd.value) {
+									s[tags[0]][tags[1]] = fd.value;
+								}
+							}
+							else
+								if (s[fd.name] != fd.value) {
+									s[fd.name] = fd.value;
+								}
+						}
+					});
+					// TODO: make effecient
+					// break;
+					// console.log(s)
+					target_sn_list.push();
+				}
+			});
+		}
+	}
 	/* Set test config to editor and highlight changed lines */
 	dhcp_config.setValue(JSON.stringify(test_config_file, null, 4), -1);
 	highlightEditedLineNumbers(dhcp_config, config_copy);
@@ -139,9 +145,9 @@ function test_dhcp_config() {
 	});
 }
 
-function upload_dhcp_config() {
-	// Push updated config file data as query string to server 
-	var affirm = confirm("Confirm: Apply changed config to server?");
+function upload_dhcp_config(svr_tag) {
+	// Push updated config file data as query string to  
+	var affirm = confirm("Confirm: Apply changed config to " + svr_tag + " server?");
 	params = "mode=update&affirm=" + affirm + "&dhcp_config_file=" + encodeURIComponent(dhcp_config.getValue());
 
 	$.post("/dhcp_config_update", params, function (data) {
--- routes/dhcp_config.js
+++ routes/dhcp_config.js
@@ -13,80 +13,95 @@ router.get('/', authorize.auth, function (req, res, next) {
 	content = template_render.set_template_variable(content, "dhcp_config_content", JSON.stringify(kea_config, null, 4));
 	content = template_render.set_template_variable(content, "title", anterius_config.current_server.toUpperCase().replace('P', 'Pv'));
 
-	var nw_type = req.query.network;
-	var nw_id = req.query.id;
+	var nw_id, nw_type = req.query.network;
+	if (req.query.id)
+		nw_id = req.query.id;
+	else
+		nw_id = -1;
 
 	if (nw_type) {
-		if (nw_type == 'reservations') {
 
-			var nw_entity;
-			hr_addr = req.query.id.split(':')[0];
-			subnet_id = req.query.id.split(':')[1];
-			subnet_list.forEach(s => {
-				if (s.id == subnet_id) {
-					// pools = s.pools;
-					s['reservations'].forEach(h => {
-						if (h['ip-address'] == hr_addr)
-							nw_entity = h;
-					});
+		if (req.query.mode == 'edit') {
+			if (nw_type == 'reservations') {
 
-					// TODO: make effecient
-					// break;
-				}
-			});
-
-			content = template_render.set_template_variable(content, "edit_title", "Host Reservation [ " + nw_entity['ip-address'] + " ] Configuration options");
-
-			input = template_render.form_input('IP Address', '<input type="text" class="form-control" name="ip-address" id="ip-address" placeholder="Enter address to be reserved" value="' + nw_entity['ip-address'] + '">');
-			input = input + template_render.form_input('Hostname', '<input type="text" class="form-control" name="hostname" id="hostname" placeholder="Enter device hostname" value="' + nw_entity.hostname + '">');
-			input = input + template_render.form_input('Server-Hostname', '<input type="text" class="form-control" name="server-hostname" id="server-hostname" placeholder="Enter hostname for server" value="' + nw_entity['server-hostname'] + '">');
-			input = input + template_render.form_input('Hardware Address', '<input type="text" class="form-control" name="hw-address" id="hw-address" placeholder="Enter MAC address for device" value="' + nw_entity['hw-address'] + '">');
-			input = input + template_render.form_input('Next Server', '<input type="text" class="form-control" name="next-server" id="next-server" placeholder="Enter next server address" value="' + nw_entity['next-server'] + '">');
-
-		}
-		else {
-
-			var nw_entity;
-			if (nw_type == server.sn_tag) {
-
+				var nw_entity;
+				hr_addr = req.query.id.split(':')[0];
+				subnet_id = req.query.id.split(':')[1];
 				subnet_list.forEach(s => {
-					if (s.id == req.query.id) {
+					if (s.id == subnet_id) {
 						// pools = s.pools;
-						nw_entity = s;
+						s['reservations'].forEach(h => {
+							if (h['ip-address'] == hr_addr)
+								nw_entity = h;
+						});
+
 						// TODO: make effecient
 						// break;
 					}
 				});
 
-				content = template_render.set_template_variable(content, "edit_title", "Subnet ID : " + nw_entity.id + " [ <a href='/nw_detail_info?type=" + server.sn_tag + "&id=" + nw_entity.id + "'>" + nw_entity.subnet + "</a> ] Configuration options");
-				input = template_render.form_input('Subnet', '<input type="text" class="form-control" name="subnet" id="subnet" placeholder="Enter address/netmask" value="' + nw_entity.subnet + '">');
+				content = template_render.set_template_variable(content, "edit_title", "Host Reservation [ " + nw_entity['ip-address'] + " ] Configuration options");
+
+				input = template_render.form_input('IP Address', '<input type="text" class="form-control" name="ip-address" id="ip-address" placeholder="Enter address to be reserved" value="' + nw_entity['ip-address'] + '">');
+				input = input + template_render.form_input('Hostname', '<input type="text" class="form-control" name="hostname" id="hostname" placeholder="Enter device hostname" value="' + nw_entity.hostname + '">');
+				input = input + template_render.form_input('Server-Hostname', '<input type="text" class="form-control" name="server-hostname" id="server-hostname" placeholder="Enter hostname for server" value="' + nw_entity['server-hostname'] + '">');
+				input = input + template_render.form_input('Hardware Address', '<input type="text" class="form-control" name="hw-address" id="hw-address" placeholder="Enter MAC address for device" value="' + nw_entity['hw-address'] + '">');
+				input = input + template_render.form_input('Next Server', '<input type="text" class="form-control" name="next-server" id="next-server" placeholder="Enter next server address" value="' + nw_entity['next-server'] + '">');
 			}
 			else {
 
-				server.server_config[server.svr_tag]['shared-networks'].forEach(s => {
-					if (s.name == req.query.id) {
-						// s[server.sn_tag].forEach(x => {
-						// 	id.push(x['id']);
-						// });
-						nw_entity = s;
-					}
-					// TODO: make effecient
-					// break;
-				});
+				var nw_entity;
+				if (nw_type == server.sn_tag) {
 
-				content = template_render.set_template_variable(content, "edit_title", "Shared Network [ <a href='/nw_detail_info?type=shared-networks&id=" + nw_entity.name + "'>" + nw_entity.name + "</a> ] Configuration options");
-				input = template_render.form_input('Shared NW name', '<input type="text" class="form-control"  name="name" id="name" placeholder="Enter shared network name" value="' + nw_entity.name + '">');
+					subnet_list.forEach(s => {
+						if (s.id == req.query.id) {
+							// pools = s.pools;
+							nw_entity = s;
+							// TODO: make effecient
+							// break;
+						}
+					});
+
+					content = template_render.set_template_variable(content, "edit_title", "Subnet ID : " + nw_entity.id + " [ <a href='/nw_detail_info?type=" + server.sn_tag + "&id=" + nw_entity.id + "'>" + nw_entity.subnet + "</a> ] Configuration options");
+					input = template_render.form_input('Subnet', '<input type="text" class="form-control" name="subnet" id="subnet" placeholder="Enter address/netmask" value="' + nw_entity.subnet + '">');
+				}
+				else {
+
+					server.server_config[server.svr_tag]['shared-networks'].forEach(s => {
+						if (s.name == req.query.id) {
+							// s[server.sn_tag].forEach(x => {
+							// 	id.push(x['id']);
+							// });
+							nw_entity = s;
+						}
+						// TODO: make effecient
+						// break;
+					});
+
+					content = template_render.set_template_variable(content, "edit_title", "Shared Network [ <a href='/nw_detail_info?type=shared-networks&id=" + nw_entity.name + "'>" + nw_entity.name + "</a> ] Configuration options");
+					input = template_render.form_input('Shared NW name', '<input type="text" class="form-control"  name="name" id="name" placeholder="Enter shared network name" value="' + nw_entity.name + '">');
+				}
+
+				// Subnet, Shared nw common parameters
+				input += template_render.form_input('Valid Lifetime', '<input type="number" class="form-control" name="valid-lifetime" id="valid-lifetime" placeholder="Enter valid lifetime" value="' + nw_entity['valid-lifetime'] + '">');
+				input += template_render.form_input('Renew Timer', '<input type="number" class="form-control" name="renew-timer" id="renew-timer" placeholder="Enter renew time interval" value="' + nw_entity['renew-timer'] + '">');
+				input += template_render.form_input('Rebind Timer', '<input type="number" class="form-control" name="rebind-timer"  id="rebind-timer" placeholder="Enter rebindtime interval" value="' + nw_entity['rebind-timer'] + '">');
+				input += template_render.form_input('Relay IP Address', '<input type="text" class="form-control" name="relay_ip-addresses" id="relay_ipaddr" placeholder="Enter relay address" value="' + nw_entity.relay['ip-address'] + '">');
 			}
+		} else {
+			content = template_render.set_template_variable(content, "edit_title", "Shared Network Creation options");
+			input = template_render.form_input('Shared NW name', '<input type="text" class="form-control"  name="name" id="name" placeholder="Enter shared network name">');
 
 			// Subnet, Shared nw common parameters
-			input += template_render.form_input('Valid Lifetime', '<input type="number" class="form-control" name="valid-lifetime" id="valid-lifetime" placeholder="Enter valid lifetime" value="' + nw_entity['valid-lifetime'] + '">');
-			input += template_render.form_input('Renew Timer', '<input type="number" class="form-control" name="renew-timer" id="renew-timer" placeholder="Enter renew time interval" value="' + nw_entity['renew-timer'] + '">');
-			input += template_render.form_input('Rebind Timer', '<input type="number" class="form-control" name="rebind-timer"  id="rebind-timer" placeholder="Enter rebindtime interval" value="' + nw_entity['rebind-timer'] + '">');
-			input += template_render.form_input('Relay IP Address', '<input type="text" class="form-control" name="relay_ip-addresses" id="relay_ipaddr" placeholder="Enter relay address" value="' + nw_entity.relay['ip-address'] + '">');
+			input += template_render.form_input('Valid Lifetime', '<input type="number" class="form-control" name="valid-lifetime" id="valid-lifetime" placeholder="Enter valid lifetime" >');
+			input += template_render.form_input('Renew Timer', '<input type="number" class="form-control" name="renew-timer" id="renew-timer" placeholder="Enter renew time interval" >');
+			input += template_render.form_input('Rebind Timer', '<input type="number" class="form-control" name="rebind-timer"  id="rebind-timer" placeholder="Enter rebindtime interval" >');
+			input += template_render.form_input('Relay IP Address', '<input type="text" class="form-control" name="relay_ip-addresses" id="relay_ipaddr" placeholder="Enter relay address" >');
 		}
 
-		input += '<div class="row" align="center"><button type="button" id="gen_btn" class="btn btn-info waves-effect ant-btn" disabled style="margin-bottom: 2%; width: 25%;" onclick=\'gen_dhcp_config('
-			+ JSON.stringify(server) + ',"' + nw_id + '","' + nw_type + '",' + JSON.stringify(subnet_list) + ')\'><i class="material-icons">settings</i> <span>Write Changes to Test file</span></button></div>';
+		/* Config gen button */
+		input += '<div class="row" align="center"><button id="gen_btn" type="button" class="btn btn-info waves-effect ant-btn" style="margin-bottom: 2%; width: 25%;" onclick=\'gen_dhcp_config("'
+			+ req.query.mode + '","' + server.svr_tag + '","' + server.sn_tag + '","' + nw_id + '","' + nw_type + '",' + JSON.stringify(subnet_list) + ')\'><i class="material-icons">settings</i> <span>Write ' + req.query.mode + ' changes to Test file</span></button></div>';
 
 		form_data = template_render.form_body("config-form", input);
 
