0fafcff82bcff6c138af49f76fe7397573afd477
Anthrino <jerinjohn101@gmail.com>
2018-07-23 14:04:24 +0530

    Add local server counter param test beta

--- tests/test.js
+++ tests/test.js
@@ -4,8 +4,8 @@ var expect = require('chai').expect;
 var api_agent = require('./lib/api_service.js');
 var json_file = require('jsonfile');
 
-describe('Anterius Config Validation', function () {
-  it('should validate config file exists', function () {
+describe('Anterius Config Validation', () => {
+  it('should validate config file exists', () => {
     expect('config/anterius_config').to.be.a.file('Anterius config file found.');
   });
   anterius_config = json_file.readFileSync('config/anterius_config.json');
@@ -15,6 +15,24 @@ describe('Anterius Config Validation', function () {
     });
   });
 });
+
+describe('Local Server Counter Information', () => {
+  it('should validate counter info fetched from local command execution', () => {
+    if (anterius_config.server_addr == 'localhost') {
+      host_name = execSync("cat /etc/hostname").toString().replace("\n", "");
+      expect(host_name).to.be.a('string');
+
+      /* Split run status and check if active/inactive */
+      run_status = execSync("keactrl status").toString().toLowerCase().split('\n');
+      expect(run_status).to.be.an('array');
+      run_status.should.all.include('active');
+
+      cpu_utilization = parseFloat(execSync("top -bn 1 | awk 'NR>7{s+=$9} END {print s/4}'"));
+      expect(cpu_utilization).to.be('float');
+    }
+
+  });
+})
 
 describe('fire_kea_api()', function () {
   it('Fetch data from API using request argument passed', function () {
