70397f24860a67b0d9e08f046ed2b54fe97f5dad
Anthrino <jerinjohn101@gmail.com>
2018-07-24 23:39:37 +0530

    Add API call test fn beta

--- tests/test.js
+++ tests/test.js
@@ -34,16 +34,42 @@ describe('Local Server Counter Information', () => {
   });
 })
 
-describe('fire_kea_api()', function () {
-  it('Fetch data from API using request argument passed', function () {
-
+describe('API mechanism - fire_kea_api()', function () {
+  
+  function fire_api(request){
+    api_agent.fire_kea_api(request, anterius_config.server_addr, anterius_config.server_port).then(function (api_data) {
+      if (api_data.result == 0){
+        expect(api_data).to.be.an('object');
+        return api_data.arguments;
+      }
+      else
+        console.log("CA Error: " + api_data.text);
+    }).catch(function () {
+      console.log("Kea CA API error");
+    });
+  }
+  
+  it('should fetch stats data from API ', function () {
     stats_req_data = JSON.stringify({ "command": "statistic-get-all", "service": ["dhcp4"] });
+    stats = fire_api(stats_req_data);
+    expect(stats).should.all.
+  });
+
+  it('should fetch config data from API ', function () {
     config_get_req_data = JSON.stringify({ "command": "config-get", "service": ["dhcp4"] });
+  });
+
+  it('should fetch stats data from API ', function () {
     lease_get_req_data = JSON.stringify({ "command": "lease4-get-all", "service": ["dhcp4"], "arguments": { "subnets": id } });
+  });
+
+  it('should fetch stats data from API ', function () {
     config_test_req_data = JSON.stringify({ "command": "config-test", "service": ["dhcp4"], "arguments": JSON.parse(request.dhcp_config_file) });
 
     response_data = api_agent.fire_kea_api()
     expect(sum2).to.be.equal(sum1);
 
   });
+
+  
 });
