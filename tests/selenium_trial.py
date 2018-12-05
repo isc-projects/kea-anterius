1e6a61910cf32d831326b81b28928a6883854a53
Anthrino <jerinjohn101@gmail.com>
2018-12-05 13:58:50 +0530

    Update alert settings test fn - selenium

--- tests/selenium_trial.py
+++ tests/selenium_trial.py
@@ -1,5 +1,5 @@
 '''
-©Anthrino / Selenium test script trial for Anterius
+©Anthrino / Browser automation test script for Anterius UI using Selenium
 
 '''
 
@@ -10,180 +10,55 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 
+import os
 import json
 import requests
+import time
 
-config_request_url = "" 
-
-def browser_open(browser, link):
-	browser.get(link)
-
-def (browser, username, passwd):
-	
-	browser.get("http://localhost:3000")
-	assert "Kea-Anterius" in browser.title
-
-	elem = browser.find_element_by_id("email")
-	elem.send_keys(username)
-
-	elem = browser.find_element_by_id("pass")
-	elem.send_keys(passwd)
-
-	elem.send_keys(Keys.RETURN)
-
-	wait = WebDriverWait(browser, 25)
-	  
-	try:
-
-		page_loaded = wait.until( lambda browser: "login.php?login_attempt" in browser.current_url)
-		
-		if browser.current_url == 'https://www.facebook.com':
-	 		print("Successful Login")
-
-	except TimeoutException:
-		print( "Loading timeout expired" )
+config_request_url = ""
+script_dir = os.path.dirname(__file__)
 
 
-def verify_table_entries(server):
+def alert_settings(browser):
 
-	with open('../config/anterius_config.json') as f:
-		ant_config = json.load(f)
+    browser.get("http://localhost:3000/anterius_alerts")
 
-	config_request_url = 'http://'+ant_config.server_addr+':'+ant_config.server_port 
+    # authenticate(browser)
 
-	data = { "command": "config-get", "service": [ server ] }
- 
-	resp = requests.post(url = config_request_url, data = data)
-	# print(resp.arguments)
-	server_config = resp.arguments
+    sn_critical = browser.find_element_by_id(
+        "shared_network_critical_threshold")
+    sn_warn = browser.find_element_by_id("shared_network_warning_threshold")
+    lpm_thresh = browser.find_element_by_id("leases_per_minute_threshold")
+    save_btn = browser.find_element_by_class_name("ant-btn")
 
-	data =  {"command": "statistic-get-all", "service": [ server ] }
-	resp = requests.post(url = config_request_url, data = data)
-	# print(resp.arguments)
-	server_stats = resp.arguments
+    snc = sn_critical.get_attribute("value")
+    snw = sn_warn.get_attribute("value")
+    lpmth = lpm_thresh.get_attribute("value")
 
-	// Generate subnet and shared nw lists from API for cross validationn
-	subnet_list = [], sharednw_list = []
-	for shnw in server_config[server]['shared-networks']:
-		sharednw_list.append(shnw)
-		for x in shnw[server.sn_tag]:
-			x['shared_nw_name'] = shnw.name
-			subnet_list.appendn(x)
+    sn_critical.send_keys("101")
+    sn_warn.send_keys("101")
+    lpm_thresh.send_keys("101")
 
-	for x in server_config[server]['subnet'+server.replace('dhcp')]:
-		subnet_list.append(x)
-	
-	
-	browser.get("http://localhost:3000/")
+    browser.execute_script("arguments[0].click();", save_btn)
 
-	table = driver.find_element_by_xpath("//table[@id='shared-networks']")
-	rows = table.find_element_by_xpath(".//tr")
-	for i in len(rows):
-		cols = rows[i].find_elements_by_xpath(".//td"):
-		assert cols[0].text() == sharednw_list[i].name, "Shared NW name mismatch" 
-		assert cols[1].text() == sharednw_list[i]., "Shared NW name mismatch" 
-		assert cols[2].text() == sharednw_list[i].name, "Shared NW name mismatch" 
-		assert cols[0].text() == sharednw_list[i].name, "Shared NW name mismatch" 
-			
+    with open(os.path.join(script_dir, '../config/anterius_config.json')) as f:
+        ant_config = json.load(f)
 
+    # print(ant_config)
 
-def nw_config_edit(nw_type, nw_id):
+    assert ant_config['shared_network_critical_threshold'] == "101" and ant_config[
+        'shared_network_warning_threshold'] == "101" and ant_config[
+            'leases_per_minute_threshold'] == "101", "Alert Settings update failed"
 
-	config_request_url = "" 
-	browser.get("http://localhost:3000/")
-	table = driver.find_element_by_xpath("//table[@id='"+nw_type+"']")
-	row = table.find_elements_by_xpath(".//tr")[nw_id]:
-	for td in row.find_elements_by_xpath(".//td")
+    browser.execute_script("arguments[0].value = '" + snc + "';", sn_critical)
+    browser.execute_script("arguments[0].value = '" + snw + "';", sn_warn)
+    browser.execute_script("arguments[0].value = '" + lpmth + "';", lpm_thresh)
 
-def google_search_results(browser, search_phrase, n_results):
-
-	browser.get("http://www.google.com")
-	search_field = browser.find_element_by_id("lst-ib")
-	search_field.clear()
-
-	search_field.send_keys(search_phrase)
-	search_field.submit()
-
-	browser.implicitly_wait(10)
-
-	page_list = browser.find_elements_by_xpath("//div[@class='rc']/h3[@class='r']/a")
-	count = len(page_list)
-
-	if count < n_results:
-		n_results = count
-
-	print(str(count), 'results retrieved. \nTop', n_results, 'results: \n')
-
-	for i in range(n_results):
-	   print(page_list[i].text)
-
-
-def custom_website_test(browser, url, mode):
-
-	browser.get(url)
-	
-	if mode == 'login':
-
-		input_field = browser.find_element_by_name('username')
-		input_field.send_keys('stinson@gnb.com')
-
-		input_field = browser.find_element_by_name('password')
-		input_field.send_keys('legendary')
-
-		b_submit = browser.find_element_by_id("loginfrm")
-		b_submit.submit()
-
-
-	if mode == 'signup':
-
-		input_field = browser.find_element_by_name('firstname')
-		input_field.send_keys('Barney')
-
-		input_field = browser.find_element_by_name('lastname')
-		input_field.send_keys('Stinson')
-
-		input_field = browser.find_element_by_name('phone')
-		input_field.send_keys('9564857764')
-
-		input_field = browser.find_element_by_name('email')
-		input_field.send_keys('stinson@gnb.com')
-
-		input_field = browser.find_element_by_name('password')
-		input_field.send_keys('legendary')
-
-		input_field = browser.find_element_by_name('confirmpassword')
-		input_field.send_keys('legendary')
-
-		b_submit = browser.find_element_by_id("headersignupform")
-		b_submit.submit()
-
-
-	if mode == 'nav':
-
-		option = input('Entered desired tab name: ')
-
-		b_selection = browser.find_element_by_xpath("//a[@class='loader']/span[.='"+option+"  ']/..")
-		b_selection.click()
-
+    browser.execute_script("arguments[0].click();", save_btn)
 
 if __name__ == '__main__':
 
-	# browser = webdriver.Chrome()
-	browser = webdriver.Firefox()
+    browser = webdriver.Chrome(
+        'D:\Software\chromedriver_win32\chromedriver.exe')
 
-	nw_config_edit('shared-networks', 1)
-	nw_config_edit('shared-networks', 2)
-
-	nw_config_edit('subnets', 1)
-	nw_config_edit('subnets', 2)
-
-
-	# browser_open('www.google.com')
-	# fb_login_check(browser, 'jerinjohn101@gmail.com', '...')
-
-	# google_search_results(browser, 'Urus', 10)
-
-	# custom_website_test(browser, 'http://www.phptravels.net/register', 'signup')
-	# custom_website_test(browser, 'http://www.phptravels.net/login', 'login')
-	custom_website_test(browser, 'http://www.phptravels.net', 'nav')
-	
+    alert_settings(browser)
